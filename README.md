9df4105dd908c005b1c672e80ef7fe7379331f58
Shi-Kwan <sktan@insytecg.com>
2017-12-30 12:07:12 -0500

    got the code to work multiplayer mode

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # RPS-Multiplayer
+
+
+Variable:
+P1_Occupied
+P2_Occupied
+
+
+
+Planning:
+
+
+Browser Scenario:
+	First load:
+		- Check for player 1 
+			- if exist, 
+				- pull name from user and change the label.
+				- hide p1 waiting screen
+				- show score
+			- else, 
+				- show p1 waiting screen
+				- hide score
+
+
+	Player 1/ Player 2 takes a spot
+		- P1.ref(child_added), has child("name")
+			- pull name from user and change the label.
+			- hide p1 waiting screen			
+			- show p1 score
+
+		- P2.ref(child_added), has child("name")
+			- pull name from user and change the label.
+			- hide p2 waiting screen			
+			- show p2 score
--- assets/js/app.js
+++ assets/js/app.js
@@ -34,6 +34,7 @@ var p1loss = 0;
 var p2loss = 0;
 var p1Control = false;
 var p2Control = false;
+var p1HasPlay = false, p2HasPlay = false, p1HasName = false, p2HasName = false;
 
 function hideP1Stuffs(){
 	if(p2Control ){
@@ -58,128 +59,180 @@ function initialization(){
 	$(".p2-wait-for-p1-container").hide();
 	$(".play1-container").hide();
 	$(".play2-container").hide();
+	checkForP1();
+	checkForP2();
 }
 
-initialization()
 
-p1Ref.on("value",function(snapshot){
-	console.log("printing p1 ref");
-	console.log(snapshot.val());
-	if(snapshot.val()){
-		player1.exist = true;
-		player1.name = snapshot.val().name.trim();
-		$(".p1-wait-container").hide();
-		$(".lblP1Name").text(snapshot.val().name.trim());
-		if(snapshot.val().play !==""){
-			player1.play = snapshot.val().play;
+function checkForP1(){
+	p1Ref.once("value", function(snapshot){
+		console.log("in check p1 func");
+		if(snapshot.hasChild("name")){
+			if(p1Control){
+				$(".register-container").hide();
+			}
+			console.log("found child with name");
+			$(".lblP1Name").text(snapshot.val().name.trim());
+			$("#lblP1Record").text("Win : " + snapshot.val().win + " | Loss : " + snapshot.val().loss);
+			$(".p1-wait-container").hide();
+			player1.exist = true;
+			p1Control= false;
 		}
-	}else{
-		player1.exist=false;
+	});
+}
+
+function checkForP2(){
+	p2Ref.once("value", function(snapshot){
+		console.log("in check p2 func");
+		if(snapshot.hasChild("name")){
+			console.log("found child with name");
+			$(".lblP2Name").text(snapshot.val().name.trim());
+			$("#lblP2Record").text("Win : " + snapshot.val().win + " | Loss : " + snapshot.val().loss);
+			$(".p2-wait-container").hide();
+			player2.exist = true;
+			p2Control= false;
+		}
+	})
+}
+
+p1Ref.on("child_removed", function(snapshot){
+	console.log("in child removed p1");
+	if(!snapshot.hasChild("name")){
+		console.log(" name has been removed");
+		$(".lblP1Name").text("");		
 		$(".p1-wait-container").show();
-		$(".play1-container").hide();
+	}else{
+		console.log("still has child");
 	}
-	
-},function(error){
-	console.log("error: " + error.code);
 })
 
-p2Ref.on("value",function(snapshot){
-	console.log("printing p2 ref");
-	console.log(snapshot.val());
-	if(snapshot.val()){
-		player2.exist = true;
-		player2.name = snapshot.val().name;
-		$(".p2-wait-container").hide();
-		$(".lblP2Name").text(snapshot.val().name.trim());
-		if(snapshot.val().play !==""){
-			player2.play = snapshot.val().play;
-		}
-	}else{
-		player2.exist=false;
+p2Ref.on("child_removed", function(snapshot){
+	if(!snapshot.hasChild("name")){
+		console.log("name has been removed");
+		$(".lblP2Name").text("");
 		$(".p2-wait-container").show();
+	}else{
+		console.log("still has child");		
+	}
+})
+
+database.ref().on("child_changed", function(snapshot){
+	
+	if(snapshot.key == "player1"){
+		console.log("player 1 has value change");
+		$("#lblP1Record").text("Win : " + snapshot.val().win + " | Loss : " + snapshot.val().loss);
+		if(snapshot.val().play != ""){
+			player1.play = snapshot.val().play;
+			console.log("set p1 has play to true");
+			p1HasPlay = true;
+		}else{
+			console.log("set p1 has play to false");
+			p1HasPlay = false;
+		}
+	}
+	else if(snapshot.key == "player2"){
+		console.log("player 2 has value change");
+		$("#lblP2Record").text("Win : " + snapshot.val().win + " | Loss : " + snapshot.val().loss);
+		if(snapshot.val().play != ""){
+			player2.play = snapshot.val().play;
+			console.log("set p2 has play to true");
+			p2HasPlay = true;
+		}else{
+			console.log("set p2 has play to false");
+			p2HasPlay = false;
+		}
+	}
+	console.log("p1 has play and p2 has play " + p1HasPlay + " , " + p2HasPlay);
+	if(p1HasPlay && p2HasPlay){
+		console.log("both has play");
+		playLogic();
+		$(".p1-wait-for-p2-container").hide();
+		$(".p2-wait-for-p1-container").hide();
+
+	}else if(p1Control && p1HasPlay && !p2HasPlay){
+		console.log("only p1 can see this");
+		$(".p1-wait-for-p2-container").show();
+		$(".play1-container").hide();
+	}else if(p2Control && !p1HasPlay && p2HasPlay){
+		console.log("only p2 can see this");
+		$(".p2-wait-for-p1-container").show();
 		$(".play2-container").hide();
 	}
-},function(error){
-	console.log("error: " + error.code);
 })
 
-p1Ref.on("child_added", function(snapshot){
-	player1.exist=false;	
-	if(snapshot.val().name !== "" || snapshot.val().name){
-		player1.name = snapshot.val().name;
-		$(".lblP1Name").text(player1.name);
-	}
-	if(snapshot.val().play !== ""){
-		player1.play = snapshot.val().play;
+database.ref().on("child_added",function(snapshot, prevChildKey){
+	console.log("lbl p1 name is " + $(".lblP1Name").val() + " . ")
+
+	if(snapshot.key == "player1"){
+		console.log("Name: " + snapshot.val().name);
+		console.log("Win: " + snapshot.val().win);
+		console.log("Loss: " + snapshot.val().loss);
+		console.log("Play: " + snapshot.val().play);
+		$(".lblP1Name").text(snapshot.val().name.trim());
+		$("#lblP1Record").text("Win : " + snapshot.val().win + " | Loss : " + snapshot.val().loss);
+		$(".p1-wait-container").hide();
+	}else if(snapshot.key == "player2"){
+		console.log("Name: " + snapshot.val().name);
+		console.log("Win: " + snapshot.val().win);
+		console.log("Loss: " + snapshot.val().loss);
+		console.log("Play: " + snapshot.val().play);
+		$(".lblP2Name").text(snapshot.val().name.trim());
+		$("#lblP2Record").text("Win : " + snapshot.val().win + " | Loss : " + snapshot.val().loss);
+		$(".p2-wait-container").hide();
 	}
 });
 
-p2Ref.on("child_added", function(snapshot){
-	
-	player2.exist=false;
-	if(snapshot.val().name !== "" || snapshot.val().name){
-		player2.name = snapshot.val().name;
-		$(".lblP2Name").text(player2.name);
-	}
-	if(snapshot.val().play !== ""){
-		player2.play = snapshot.val().play;
-	}
-});
 
+initialization()
 $("#cmdPlay").click(function(){
+	initialization();
 	if(player1.exist == false && player2.exist == false){
 		//both players DNE
 		// proceed by registering player1 in player1 ref
 		p1Ref.set({
-			name : $("#txtName").val()
+			name : $("#txtName").val(),
+			win : 0,
+			loss : 0,
+			play : ""
 		});
 		player1.name = $("#txtName").val();
 		p1Ref.on("child_added", function(snapshot){
 			p1Ref.onDisconnect().remove();
-			player1.exist=false;
-			if(snapshot.val().play !== ""){
-				player1.play = snapshot.val().play;
-			}
+			player1.exist = false;
 		});
 		p1Control = true;
-		hideP2Stuffs();
-		$(".player2-container").hide();
 	}else if(player1.exist == true && player2.exist == false){
 		p2Ref.set({
-			name : $("#txtName").val()
+			name : $("#txtName").val(),
+			win : 0,
+			loss : 0,
+			play : ""
 		});
 		player2.name = $("#txtName").val();
 		p2Ref.on("child_added", function(snapshot){
 			p2Ref.onDisconnect().remove();
 			player2.exist=false;
-			if(snapshot.val().play !== ""){
-				player2.play = snapshot.val().play;
-			}
 		});
-		p2Control = true;
-		hideP1Stuffs();
-		$(".player1-container").hide();
-
-		
+		p2Control = true;		
 		//player 1 EXIST
 		// proceed by registering player 2 in player 2 ref
 	}else if(player1.exist == false && player2.exist == true){
 		//player 2 EXIST
 		// proceed by register player 1 in player 1 ref
 		p1Ref.set({
-			name : $("#txtName").val()
+			name : $("#txtName").val(),
+			win : 0,
+			loss : 0,
+			play: ""
+
 		});
 		player1.name = $("#txtName").val();
 		p1Ref.on("child_added", function(snapshot){
 			p1Ref.onDisconnect().remove();
 			player1.exist=false;
-			if(snapshot.val().play !== ""){
-				player1.play = snapshot.val().play;
-			}
 		});
 		p1Control = true;
-		hideP2Stuffs();
-		$(".player2-container").hide();
+		
 	}else if(player1.exist == true && player2.exist == true){
 		//both players EXIST
 		// prompt error message "Players are playing!"
@@ -199,15 +252,21 @@ $(".btn-p1").click(function(){
 	player1.play = $(this).attr("data-play");
 	p1Ref.set({
 		name: player1.name.trim(),
-		play: $(this).attr("data-play")
+		play: $(this).attr("data-play"),
+		win : player1.win,
+		loss : player1.loss
 	});
-	p2Ref.once("value", function(snapshot){
+	/*p2Ref.once("value", function(snapshot){
 		if(snapshot.val().play){
 			$(".play1-container").hide();
 			$(".p1-wait-for-p2-container").show();
 		}
 		playLogic();
-	});
+		$("#lblP1Record").text("Win: " + p1win + "   | Loss : " + p1loss);
+		$("#lblP2Record").text("Win: " + p2win + "   | Loss : " + p2loss);
+		$("#lblP1Record").text("Win: " + p1win + "   | Loss : " + p1loss);
+		$("#lblP2Record").text("Win: " + p2win + "   | Loss : " + p2loss);
+	});*/
 })
 
 $(".btn-p2").click(function(){
@@ -215,16 +274,53 @@ $(".btn-p2").click(function(){
 	player2.play = $(this).attr("data-play");
 	p2Ref.set({
 		name: player2.name.trim(),
-		play: $(this).attr("data-play")
+		play: $(this).attr("data-play"),
+		win : player2.win,
+		loss : player2.loss
 	});
-	p1Ref.once("value", function(snapshot){
+	/*p1Ref.once("value", function(snapshot){
 		if(snapshot.val().play){
 			$(".play2-container").hide();
 			$(".p2-wait-for-p1-container").show();
 		}
 		playLogic();
-	});
+		$("#lblP1Record").text("Win: " + p1win + "   | Loss : " + p1loss);
+		$("#lblP2Record").text("Win: " + p2win + "   | Loss : " + p2loss);
+		$("#lblP1Record").text("Win: " + p1win + "   | Loss : " + p1loss);
+		$("#lblP2Record").text("Win: " + p2win + "   | Loss : " + p2loss);
+	});*/
 })
+
+function chatty(){
+	var message;
+	if(p1Control){
+		console.log("here");
+		message = player1.name + " : " + $("#txtChat").val();
+		chat.push({
+			"chat" : message,
+		});
+	}else if(p2Control){
+		message = player2.name + " : " + $("#txtChat").val();
+		chat.push({
+			"chat" : message,
+		})
+	}else{
+
+	}
+	$("#txtChat").empty();
+}
+
+$("#btnChat").click(function(){
+	event.preventDefault();
+	chatty();
+});
+
+chat.on("child_added", function(snapshot){
+	console.log("message");
+	console.log(snapshot.val().chat);
+	$("#textAreaChat").append(snapshot.val().chat);
+	$("#textAreaChat").append("\r\n");
+});
 
 function showHands(){
 	if(player1.name.trim() !== "" && player2.name.trim() !==""){
@@ -242,16 +338,11 @@ function playLogic(){
 			$(".msg-center").text("player 1 win!");
 			p1win++;
 			p2loss++;
-			$("#lblP1Record").text("Win: " + p1win + "   | Loss : " + p1loss);
-			$("#lblP2Record").text("Win: " + p2win + "   | Loss : " + p2loss);
-
 		}else if((player2.play == "Rock" && player1.play == "Scissors") || (player2.play =="Scissors" && player1.play =="Paper") || (player2.play =="Paper" && player1.play == "Rock")){
 			console.log("player 2 win!");
 			p1loss++;
 			p2win++;
 			$(".msg-center").text("player 2 win!");
-			$("#lblP1Record").text("Win: " + p1win + "   | Loss : " + p1loss);
-			$("#lblP2Record").text("Win: " + p2win + "   | Loss : " + p2loss);
 		}else if((player1.play == "Rock" && player2.play == "Rock") || (player1.play == "Scissors" && player2.play == "Scissors") ||(player1.play == "Paper" && player2.play == "Paper")){
 			console.log("ties!");
 			$(".msg-center").text("ties");
@@ -259,8 +350,7 @@ function playLogic(){
 			p1loss = p1loss;
 			p2win = p2win;
 			p2loss = p2loss;
-			$("#lblP1Record").text("Win: " + p1win + "   | Loss : " + p1loss);
-			$("#lblP2Record").text("Win: " + p2win + "   | Loss : " + p2loss);
+			
 		}
 		setScore();
 		$(".msg-center").show();
@@ -283,7 +373,8 @@ function setScore(){
 	p1Ref.set({
 		name : player1.name,
 		win : p1win,
-		loss : p1loss
+		loss : p1loss,
+		play : ""
 	});
 	player1.win = p1win;
 	player1.loss = p1loss;
@@ -291,7 +382,8 @@ function setScore(){
 	p2Ref.set({
 		name : player2.name,
 		win : p2win,
-		loss : p2loss
+		loss : p2loss,
+		play : ""
 	})
 	player2.win = p2win;
 	player2.loss = p2loss;
